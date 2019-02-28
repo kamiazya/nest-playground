ef42e94272ffcc1618760c4b410c86c535389952
kamiazya <yuki@kamiazya.tech>
2019-03-01 08:33:37 +0900

    add hoge controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HogeController } from './hoge/hoge.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, HogeController],
   providers: [AppService],
 })
 export class AppModule {}
