c70f7d31a8223764e3212c35128e30a1fae0bd1d
kamiazya <yuki@kamiazya.tech>
2019-03-01 09:10:49 +0900

    edit hoge controller

--- src/hoge/hoge.controller.ts
+++ src/hoge/hoge.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Logger } from '@nestjs/common';
 
 @Controller('hoge')
-export class HogeController {}
+export class HogeController {
+  private readonly logger = new Logger(HogeController.name);
+
+  @Get()
+  findAll() {
+    this.logger.log('aaa');
+    return 'This action returns all cats';
+  }
+}
