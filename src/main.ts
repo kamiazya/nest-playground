e3a269996c2dc42a24cf1835f825e18fcf3e87cc
kamiazya <yuki@kamiazya.tech>
2019-05-30 17:23:14 +0900

    update dependencies

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const ctx = await NestFactory.createApplicationContext(AppModule);
-  const hogeController = ctx.select(AppModule).get(HogeController, { strict: true });
+  const hogeController = ctx
+    .select(AppModule)
+    .get(HogeController, { strict: true });
   const result = await hogeController.findAll();
   const logger = new Logger();
   logger.log({ result });
